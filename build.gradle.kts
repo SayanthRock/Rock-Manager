153b501dea20656732216d1c4ec0165c46a29ea4
Sayanth Rock <202829406+SayanthRock@users.noreply.github.com>
2026-07-04 22:42:26 +0530

    Declare Kotlin and KSP plugins directly

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
   alias(libs.plugins.android.application) apply false
+  id("org.jetbrains.kotlin.android") version "2.2.10" apply false
   alias(libs.plugins.kotlin.compose) apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
+  id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
   alias(libs.plugins.roborazzi) apply false
   alias(libs.plugins.secrets) apply false
   alias(libs.plugins.google.services) apply false
